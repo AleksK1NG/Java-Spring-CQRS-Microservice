cb30e8a965a8a90f3efa418870415f03a3b72183
Alexander <alexander.bryksin@yandex.ru>
2022-04-02 13:37:35 +0300

    feature: add opentracing swagger response

--- src/main/java/com/microservice/order/queries/QueryHandler.java
+++ src/main/java/com/microservice/order/queries/QueryHandler.java
@@ -1,11 +1,10 @@
 package com.microservice.order.queries;
 
-import com.microservice.order.domain.OrderDocument;
 import com.microservice.order.dto.OrderResponseDto;
 import org.springframework.data.domain.Page;
 
 public interface QueryHandler {
     OrderResponseDto handle(GetOrderByIdQuery query);
-    Page<OrderDocument> handle(GetOrdersByUserEmailQuery query);
-    Page<OrderDocument> handle(GetOrdersByStatusQuery query);
+    Page<OrderResponseDto> handle(GetOrdersByUserEmailQuery query);
+    Page<OrderResponseDto> handle(GetOrdersByStatusQuery query);
 }
