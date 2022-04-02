24506924c0e63981a3d3ea9bf856ec4dd163802d
Alexander <alexander.bryksin@yandex.ru>
2022-04-02 13:32:45 +0300

    feature: add opentracing swagger

--- src/main/java/com/microservice/MicroserviceApplication.java
+++ src/main/java/com/microservice/MicroserviceApplication.java
@@ -1,13 +1,19 @@
 package com.microservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Spring CQRS Microservice",
+        description = "Spring Postgresql MongoDB Kafka Microservice",
+        contact = @Contact(name = "Alexander Bryksin", email = "alexander.bryksin@yandex.ru", url = "https://github.com/AleksK1NG")))
 public class MicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceApplication.class, args);
+    }
 
 }
