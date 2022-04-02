1e452fba230f23c69fa5d646a802a82d6a0f1942
Alexander <alexander.bryksin@yandex.ru>
2022-04-02 14:12:39 +0300

    feature: add sleuth zipkin

--- src/main/java/com/microservice/order/queries/OrderQueryHandler.java
+++ src/main/java/com/microservice/order/queries/OrderQueryHandler.java
@@ -1,6 +1,5 @@
 package com.microservice.order.queries;
 
-import com.microservice.order.domain.OrderDocument;
 import com.microservice.order.dto.OrderResponseDto;
 import com.microservice.order.exceptions.OrderNotFoundException;
 import com.microservice.order.mappers.OrderMapper;
@@ -8,10 +7,13 @@ import com.microservice.order.repository.OrderMongoRepository;
 import com.microservice.order.repository.OrderPostgresRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
@@ -22,29 +24,41 @@ public class OrderQueryHandler implements QueryHandler {
 
     private final OrderPostgresRepository postgresRepository;
     private final OrderMongoRepository mongoRepository;
+    private final Tracer tracer;
 
     @Override
+    @NewSpan(name = "(GetOrderByIdQuery)")
     public OrderResponseDto handle(GetOrderByIdQuery query) {
+        Optional.ofNullable(tracer.currentSpan()).map(span -> span.tag("query", query.toString()));
         final var document = mongoRepository.findById(query.id());
         if (document.isPresent()) {
+            Optional.ofNullable(tracer.currentSpan()).map(span -> span.tag("orderDocument", document.toString()));
             return OrderMapper.orderResponseDtoFromDocument(document.get());
         }
+
         final var order = postgresRepository.findById(UUID.fromString(query.id()));
         if (order.isEmpty()) throw new OrderNotFoundException("order not found: " + query.id());
 
-        OrderDocument orderDocument = OrderMapper.orderDocumentFromEntity(order.get());
+        final var orderDocument = OrderMapper.orderDocumentFromEntity(order.get());
         mongoRepository.save(orderDocument);
+        Optional.ofNullable(tracer.currentSpan()).map(span -> span.tag("orderDocument", orderDocument.toString()));
         return OrderMapper.orderResponseDtoFromEntity(order.get());
     }
 
     @Override
+    @NewSpan(name = "(GetOrdersByUserEmailQuery)")
     public Page<OrderResponseDto> handle(GetOrdersByUserEmailQuery query) {
+        Optional.ofNullable(tracer.currentSpan()).map(span -> span.tag("query", query.toString()));
+
         final var pageRequest = PageRequest.of(query.page(), query.size());
         return mongoRepository.findByUserEmailOrderByDeliveryDate(query.userEmail(), pageRequest).map(OrderMapper::orderResponseDtoFromDocument);
     }
 
     @Override
+    @NewSpan(name = "(GetOrdersByStatusQuery)")
     public Page<OrderResponseDto> handle(GetOrdersByStatusQuery query) {
+        Optional.ofNullable(tracer.currentSpan()).map(span -> span.tag("query", query.toString()));
+
         final var pageRequest = PageRequest.of(query.page(), query.size());
         return mongoRepository.findByStatusOrderByCreatedAt(query.status(), pageRequest).map(OrderMapper::orderResponseDtoFromDocument);
     }
