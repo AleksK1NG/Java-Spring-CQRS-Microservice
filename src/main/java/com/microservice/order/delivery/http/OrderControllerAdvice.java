ad093fea871b575f85a29ede8683c9c57eb9d892
Alexander <alexander.bryksin@yandex.ru>
2022-04-02 14:19:07 +0300

    feature: add sleuth zipkin tracing

--- src/main/java/com/microservice/order/delivery/http/OrderControllerAdvice.java
+++ src/main/java/com/microservice/order/delivery/http/OrderControllerAdvice.java
@@ -2,7 +2,10 @@ package com.microservice.order.delivery.http;
 
 import com.microservice.order.exceptions.OrderNotFoundException;
 import com.microservice.shared.exceptions.NotFoundExceptionResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -17,14 +20,18 @@ import java.time.ZonedDateTime;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
 @ControllerAdvice
 @Slf4j
+@RequiredArgsConstructor
 public class OrderControllerAdvice extends ResponseEntityExceptionHandler {
+    private final Tracer tracer;
 
     @ExceptionHandler(OrderNotFoundException.class)
+    @NewSpan(name = "OrderControllerAdvice")
     public ResponseEntity<NotFoundExceptionResponse> handleOrderNotFoundException(OrderNotFoundException ex, WebRequest request) {
         final var response = NotFoundExceptionResponse.builder()
                 .message(ex.getMessage())
@@ -32,7 +39,7 @@ public class OrderControllerAdvice extends ResponseEntityExceptionHandler {
                 .timestamp(ZonedDateTime.now())
                 .build();
         log.error("OrderNotFoundException response: {} ", response);
-
+        Optional.ofNullable(tracer.currentSpan()).map(span -> span.error(ex));
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
@@ -43,7 +50,7 @@ public class OrderControllerAdvice extends ResponseEntityExceptionHandler {
         body.put("timestamp", ZonedDateTime.now());
         body.put("status", status.value());
 
-        //Get all errors
+
         List<String> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
@@ -51,6 +58,7 @@ public class OrderControllerAdvice extends ResponseEntityExceptionHandler {
                 .collect(Collectors.toList());
 
         body.put("errors", errors);
+        Optional.ofNullable(tracer.currentSpan()).map(span -> span.error(ex));
 
         return new ResponseEntity<>(body, status);
     }
