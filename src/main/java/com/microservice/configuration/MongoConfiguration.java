d7fa5f0b6b581a20c615c204ef33fc901f0e7f60
Alexander <alexander.bryksin@yandex.ru>
2022-04-02 11:29:30 +0300

    feature: add microservice order mongo indexes

--- src/main/java/com/microservice/configuration/MongoConfiguration.java
+++ src/main/java/com/microservice/configuration/MongoConfiguration.java
@@ -1,17 +1,27 @@
 package com.microservice.configuration;
 
+import com.microservice.order.domain.OrderDocument;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.Index;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class MongoConfiguration {
 
+    private final MongoTemplate mongoTemplate;
 
     @PostConstruct
     public void mongoInit() {
-        log.info("MongoDB connected");
+        final var orders = mongoTemplate.getCollection("orders");
+        final var emailIndex = mongoTemplate.indexOps(OrderDocument.class).ensureIndex(new Index("userEmail", Sort.Direction.ASC));
+        final var indexInfo = mongoTemplate.indexOps(OrderDocument.class).getIndexInfo();
+        log.info("MongoDB connected, email index created: {}", indexInfo);
     }
 }
